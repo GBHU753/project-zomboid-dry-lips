da1a7840b1cd1e8deb6fa1584b625cce8c674919
GBHU753 <gbhu753@aucklanduni.ac.nz>
2023-12-06 18:36:41 +1300

    Feat: Implementation with known bugs

--- media/lua/client/DryLips.lua
+++ media/lua/client/DryLips.lua
@@ -7,7 +7,7 @@ local playerLogLimiter = false;
 
 
 --  ordering bad4, bad3, bad2, bad1,   good1, good2, good3, good4
-local moodleThreshold =  {0.1, 0.2, 0.3, 0.4,   0.6, 0.7, 0.8, 0.9};
+local moodleThreshold = { 0.1, 0.2, 0.3, 0.4, 0.6, 0.7, 0.8, 0.9 };
 
 --note that you can override thresholds with
 --MF.getMoodle("DryLips"):setThresholds(bad4, bad3, bad2, bad1, good1, good2, good3, good4)
@@ -16,62 +16,49 @@ local moodleThreshold =  {0.1, 0.2, 0.3, 0.4,   0.6, 0.7, 0.8, 0.9};
 --e.g. if bad1 == 0.3 and good == 0.365 then moodle:setValue(0.311) will remove the moodle.
 
 
-local function DryLipsMoodleUpdate(player)
-    
-
-    if player == getPlayer() then
-
-        -- guard case to check if the lip dryness value is set
-        -- if not, set it to 0
-        local myModData = player:getModData();
-        if myModData.LipDryness == nil then
-            myModData.LipDryness = 0;
-            myModData.LipDryMoodlePos = 7;
-        end
+local function DryLipsMoodleUpdate()
+    local player = getPlayer();
 
 
-        local moodle = MF.getMoodle("DryLips");--get access to the moodle with that name associated to getPlayer
-
-        
-        -- if the lip dryness is equal to the threshold, then set the moodle to the next level
-        if (myModData.LipDryness == SandboxVars.DryLipsHoursTill) then
-            
-            -- decrement the moodle position if it is not already at the lowest level
-            if (myModData.LipDryMoodlePos > 0) then
-                myModData.LipDryMoodlePos = myModData.LipDryMoodlePos - 1;
-            end
-
-            -- set the moodle to the next level
-            moodle:setValue(moodleThreshold[myModData.LipDryMoodlePos]);
-
-            -- reset the lip dryness counter
-            myModData.LipDryness = 0;
-        end
-
-        myModData.LipDryness = myModData.LipDryness + 1;
-    
-    else
-        if not playerLogLimiter then print ("ProteinsMoodleUpdate not being getPlayer "..player:getPlayerNum()) end
-        playerLogLimiter = true;
+    -- guard case to check if the lip dryness value is set
+    -- if not, set it to 0
+    local myModData = player:getModData();
+    if myModData.LipDryness == nil then
+        myModData.LipDryness = 0;
+        myModData.LipDryMoodlePos = 7;
     end
 
+
+    local moodle = MF.getMoodle("DryLips");     --get access to the moodle with that name associated to getPlayer
+
+
+    print("LipDryness: " .. myModData.LipDryness);
+    print("DryLipsHoursTill: " .. SandboxVars.DryLips.HoursTill);
+    -- if the lip dryness is equal to the threshold, then set the moodle to the next level
+    if (myModData.LipDryness == SandboxVars.DryLips.HoursTill) then
+        -- decrement the moodle position if it is not already at the lowest level
+        if (myModData.LipDryMoodlePos > 0) then
+            myModData.LipDryMoodlePos = myModData.LipDryMoodlePos - 1;
+        end
+
+        -- set the moodle to the next level
+        moodle:setValue(moodleThreshold[myModData.LipDryMoodlePos]);
+
+        -- reset the lip dryness counter
+        myModData.LipDryness = 0;
+    end
+
+    myModData.LipDryness = myModData.LipDryness + 1;
 end
 
 
 -- Event hooks
 Events.OnPlayerDeath.Add(function()
-    Events.EveryHours.Add(DryLipsMoodleUpdate);--Dry lips moodle is related to hours update (time).
---but you can use moodle for anything, not always related to player update.
+    Events.EveryHours.Add(DryLipsMoodleUpdate); --Dry lips moodle is related to hours update (time).
+    --but you can use moodle for anything, not always related to player update.
 end);
 
 Events.OnCreatePlayer.Add(function()
-    Events.EveryHours.Add(DryLipsMoodleUpdate);--Dry lips moodle is related to hours update (time).
---but you can use moodle for anything, not always related to player update.
+    Events.EveryHours.Add(DryLipsMoodleUpdate); --Dry lips moodle is related to hours update (time).
+    --but you can use moodle for anything, not always related to player update.
 end);
-
-
-
-
-
-
-
